c8003fbcce1f5a5fca9b1621519bdb94a7daa4a4
Vladislav Zakharov <vl4disalex@gmail.com>
2025-09-23 17:30:18 +0300

    Complete the second part of the site layout, including a slider with functionality and a blog section. Finished the main site layout.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 2,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: '.swiper-button-right',
+    prevEl: '.swiper-button-left',
+  },
+
+});
